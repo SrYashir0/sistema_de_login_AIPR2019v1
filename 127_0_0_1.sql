321ec899c74eaca75969347606d4a23a39957fbc
Yashirin <davi_g_santos@estudante.sc.senai.br>
2019-09-06 14:08:57 -0300

    Novo campo BD
    Criação de coluna "avatar" no BD
    
    closes #16

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23-Ago-2019 às 14:15
+-- Tempo de geração: 06-Set-2019 às 14:06
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -21,7 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `sistemadelogin`
 --
-DROP DATABASE IF EXISTS `sistemadelogin`;
 CREATE DATABASE IF NOT EXISTS `sistemadelogin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE `sistemadelogin`;
 
@@ -30,19 +29,23 @@ USE `sistemadelogin`;
 --
 -- Estrutura da tabela `usuario`
 --
--- Criação: 23-Ago-2019 às 17:10
--- Última actualização: 23-Ago-2019 às 17:10
---
 
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `idUsuario` int(10) UNSIGNED NOT NULL,
   `nome` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `nomeUsuario` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `senha` char(40) COLLATE utf8mb4_bin NOT NULL,
-  `dataCriacao` datetime NOT NULL
+  `dataCriacao` datetime NOT NULL,
+  `avatar` varchar(200) COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar`) VALUES
+(1, 'Davi Santos', 'Yashirin', 'Yashirin@sss', 'fde3e7ccf148658663a1748362bf77edf0b6a070', '2019-09-06 13:39:27', '');
 
 --
 -- Índices para tabelas despejadas
@@ -63,7 +66,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
