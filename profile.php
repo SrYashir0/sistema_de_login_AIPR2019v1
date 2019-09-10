408a7e7da408758f280d858c5228da459b83436a
SrYashir0 <davi_g_santos@estudante.sc.senai.br>
2019-09-10 16:45:24 -0300

    Criação do Sistema de Le,bre-se de mim
    closes #11

--- profile.php
+++ profile.php
@@ -19,7 +19,7 @@ require_once 'session.php';
 
 <body class="bg-dark">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Hillesheim Systems</a>
+        <a class="navbar-brand" href="#">Yashirin Systems</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
